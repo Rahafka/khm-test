d12d67587c7278ddc4c1cf2cb290cf4537a95cc2
Rahafka <rahafka95@gmail.com>
2023-04-07 17:45:41 +0300

    first git

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> 
-    <!--  ssgit  -->
+    <!--  ssgit   swkjskj-->
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
